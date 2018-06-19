c1618421f6151083852f7b2d2d6e12751f3f77d8
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-06-19 19:11:29 -0300

    testes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './login';
+import './index.css';
+import login from './login';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<login />, document.getElementById('root'));
--- src/login.js
+++ src/login.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import video from './loginvideo.mp4';
+import logo from './logo1.png';
+import './login.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+                <div className="vid-container">
+                    <video id="Video1" className="bgvid back" autoplay="autoplay" muted="muted" preload="auto" loop="">
+                        <source src={video} type="video/mp4" alt="video" />
+                    </video>
+                    <div className="inner-container">
+                        <div className="box">
+                            <img className="logo" src={logo} alt="logo"/>
+                            <input placeholder="Usuário" type="text" />
+                            <input placeholder="Senha" type="password" />
+                            <button type="button">Login</button>
+                        </div>
+                    </div>
+                </div>
+                );
+    }
 }
 
 export default App;
