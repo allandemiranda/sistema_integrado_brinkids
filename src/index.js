54e87f5a8c6a547d6f797198ee82cde2c76b0442
Allan de Miranda Silva <allandemiranda@gmail.com>
2018-06-19 18:35:05 -0300

    testando o react
    Iniciando criação da tela de login

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import App from './login';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<login />, document.getElementById('root'));
 registerServiceWorker();
