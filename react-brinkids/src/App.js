b6143ed15c7cf4fc9f0a1d6519711e5322c85d48
Allan de Miranda <allandemiranda@gmail.com>
2018-07-02 21:34:25 -0300

    edição da tela de login

--- react-brinkids/src/App.js
+++ react-brinkids/src/App.js
@@ -1,43 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo1.png';
-//import video from './loginvideo.mp4';
+import video from './loginvideo.mp4';
 import './App.css';
 
 class App extends Component {
-    state = 
-    {
-        users: []
-    }
-
-    componentDidMount() {
-      fetch('/users')
-        .then(res => res.json())
-        .then(users => this.setState({ users }));
+    constructor() {
+        super();
+        this.state = {
+            status: true,
+            erroMsg: "Usuário ou senha incorreto !"
+        };
     }
     
     render() {
         return (
                 <div className="vid-container">
-                    {
-                    //<video id="Video1" className="bgvid back" autoPlay="true" muted="muted" preload="auto" loop>
-                        //<source src={video} type="video/mp4" alt="video" />
-                    //</video>
-                    }
-                    <h1>Users</h1>
-                    {this.state.users.map(user =>
-                        <div key={user.id}>
-                            username: {user.username}
-                            <br/>
-                            password: {user.password}
-                        </div>
-                        
-                    )}
+                    <video id="Video1" className="bgvid back" autoPlay="true" muted="muted" preload="auto" loop>
+                        <source src={video} type="video/mp4" alt="video" />
+                    </video>
                     <div className="inner-container">
                         <div className="box">
                             <img className="logo" src={logo} alt="logo" />
-                            <input placeholder="Usuário" type="text" />
-                            <input placeholder="Senha" type="password" />
-                            <button type="button">Login</button>
+                            <p> 
+                                <span>
+                                {
+                                    this.state.status ? this.state.erroMsg : null
+                                }
+                                </span> 
+                            </p>
+                            <form method="get" action="/user/login">
+                                <input id="inputUser" placeholder="Usuário" type="text" />
+                                <input id="inputPassword" placeholder="Senha" type="password" />                            
+                                <button type="submit">Login</button>
+                            </form>
                         </div>
                     </div>
                 </div>
