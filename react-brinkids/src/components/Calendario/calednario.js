11dcf00191921b65713dc782adaa36739ba0dfb2
pj300e <38145047+pj300e@users.noreply.github.com>
2018-07-28 18:44:12 -0300

    #16 atualizando o componente agenda

--- react-brinkids/src/components/Calendario/calednario.js
+++ react-brinkids/src/components/Calendario/calednario.js
@@ -10,21 +10,35 @@
   import BigCalendar from 'react-big-calendar';
   import moment from 'moment';
   import 'moment/locale/pt-br';
-  import './styles/react-big-calendar.css';
+  
   import events from './events';
-  import '../../assets/style/bootstrap.min.css';
+  
   import DatePicker from 'react-date-picker';
   import Modal from 'react-modal';
   import './styles/DatePicker.css';
-  import Select from 'react-select';
-  import opcao from './opções.js';
+  
+
   import { timeFromInt } from 'time-number';
   import TimePicker from 'react-bootstrap-time-picker';
+  import '../../assets/style/font-awesome.css';
+  import estilo from './styles/react-big-calendar.css';
+
+
+
+  const estilos = {
+        
+            float: "right",
+    display: "inline-block" ,
+    width: "45% ",
+    height: "45px",
+    padding: "14px 12px",
+    fontSize: "14px",
 
 
 
 
-
+        
+  };
   const customStyles = {
     content : {
       top                   : '30%',
@@ -33,16 +47,23 @@
       bottom                : 'auto',
       marginRight           : '-50%',
       transform             : 'translate(-30%, -30%)',
-      overflow: 'visible',
-      position: 'absolute',
+      overflow              : 'visible',
+      position              : 'absolute',
 
     }
   };
+  const iconelixeira = {
+    cursor: "pointer",
+
+
+
+  };
+
   /*   */
 
   BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment));
-
-  Modal.setAppElement(Calendar);
+  var excluirInicial,excluirFinal;
+  
   class Calendar extends React.Component {
     constructor(props) {
       super(props);
@@ -55,6 +76,7 @@
         Hora: '0',
         Hora2:'0',
         Titulo:'',
+        modalC:false,
 
 
       };
@@ -62,15 +84,33 @@
       this.openModal = this.openModal.bind(this);
       this.afterOpenModal = this.afterOpenModal.bind(this);
       this.closeModal = this.closeModal.bind(this);
+      this.afterOpenModal = this.afterOpenModal.bind(this);
+
+      this.openModalC = this.openModalC.bind(this);
+      this.afterOpenModalC = this.afterOpenModalC.bind(this);
+      this.closeModalC = this.closeModalC.bind(this);
+      
+      
       this.mod = this.mod.bind(this);
       this.mudarTitulo = this.mudarTitulo.bind(this);
+      this.mudarModal = this.mudarModal.bind(this);
+
+      this.ExcluirEvento = this.ExcluirEvento.bind(this);
 
     }
 
+    componentWillMount() {
+      Modal.setAppElement('body');
+    }
+    
+
     onChange = CalendarioI => this.setState({ CalendarioI });
     onChange2 = CalendarioF => this.setState({CalendarioF});
+    
     mudarHora = Hora => this.setState({Hora});
     mudarHora2 = Hora2 => this.setState({Hora2});
+    
+
     mudarTitulo(event){
 
       this.setState({Titulo: event.target.value});
@@ -88,37 +128,73 @@
   closeModal() {
     this.setState({modalIsOpen: false});
   }
+
+  openModalC() {
+    this.setState({modalC: true});
+  }
+
+  afterOpenModalC() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModalC() {
+    this.setState({modalC: false});
+  }
   mod(){
-        const titulo = this.state.Titulo;
-        const Final = this.state.CalendarioF;
-        const Inicial = this.state.CalendarioI;
-        const Anoinicial = Inicial.getFullYear(), MesInicial= Inicial.getMonth(), Diainicial = Inicial.getDate();
-        const Anofinal = Final.getFullYear(), Mesfinal= Final.getMonth(), Diaifinal = Final.getDate();
+    const titulo = this.state.Titulo;
+    const Final = this.state.CalendarioF;
+    const Inicial = this.state.CalendarioI;
+    const Anoinicial = Inicial.getFullYear(), MesInicial= Inicial.getMonth(), Diainicial = Inicial.getDate();
+    const Anofinal = Final.getFullYear(), Mesfinal= Final.getMonth(), Diaifinal = Final.getDate();
 
-        const HoraI =timeFromInt(this.state.Hora);
-        const HoraF =timeFromInt(this.state.Hora2);
-        const match = HoraI.match(/([\w\*]+)/g);
-        const match2 = HoraF.match(/([\w\*]+)/g);
+    const HoraI =timeFromInt(this.state.Hora);
+    const HoraF =timeFromInt(this.state.Hora2);
+    const match = HoraI.match(/([\w\*]+)/g);
+    const match2 = HoraF.match(/([\w\*]+)/g);
 
-        events.push({title:titulo,start:new Date(Anoinicial,MesInicial, Diainicial, match[0], match[1], 0),end:new Date(Anofinal,Mesfinal, Diaifinal, match2[0], match2[1], 0),desc:'blabla bla'});
-        this.closeModal();
+    events.push({title:titulo,start:new Date(Anoinicial,MesInicial, Diainicial, match[0], match[1], 0),end:new Date(Anofinal,Mesfinal, Diaifinal, match2[0], match2[1], 0),desc:'blabla bla'});
+    this.setState({Titulo:''});
+    this.closeModal();
+
+  }
+
+  mudarModal(event){
+    excluirInicial = event.start;
+    excluirFinal = event.end;
+    const inicial = event.start.getHours() +':'+event.start.getMinutes();
+    const final =event.end.getHours() +':'+event.end.getMinutes();
+
+    this.setState({Titulo: event.title});
+    this.setState({Hora2:final});
+    this.setState({Hora:inicial});
+    this.openModalC();
+
+  }
+
+
+  ExcluirEvento(event){
+
+    for(var data in events ){
+
+      if(events[data].start === excluirInicial && events[data].end === excluirFinal){
+        events.splice(data, 1);
         this.setState({Titulo:''});
+        this.closeModalC();
+      }
+    }
+
+
   }
 
   render() {
 
 
-
-/*  FUNÇÃO PARA O TRATAMENTO DA DATA DE INICIO E FINAL DO EVENTOE TBM PARA ADICIONAR O TITULO DO EVENTO
-
-    OBS:  VAI SER MAIS TRABALHADA, NÃO CONSEGUI APLICAR UM MODAL, POR ENQUANTO ESTA ATUANDO COM PROMPT
-
-    */
-
     const teste =(slotInfo) =>{
 
 
     }
+
 
     return (
       /*  COMPONENTE CALENDARIO DO REACT */
@@ -126,6 +202,26 @@
       <div>
 
       <button className="modal1" type="button" onClick={this.openModal}>adicionar evento</button>
+      
+      <Modal
+      isOpen={this.state.modalC}
+      onAfterOpen={this.afterOpenModalC}
+      onRequestClose={this.closeModalC}
+      style={customStyles}
+      contentLabel="Example Modal2"
+      > 
+      <h ref={subtitle => this.subtitle = subtitle}></h>
+      <div className="glyphicon" style={iconelixeira} onClick={this.ExcluirEvento}>&#xe020;</div>
+      <div className="fa"  style={{cursor: "pointer", float:"right"}} onClick={this.closeModalC}>&#xf00d;</div><br/><br/>
+
+
+      <div>Titulo: <input type="text" className="titulo2" placeholder ="digite o titulo" value={this.state.Titulo} onChange={this.mudarTitulo}/><br/>
+      </div>  <br/> 
+      <div> De: <span>{this.state.Hora} ate {this.state.Hora2}</span></div><br/>
+      <input type="button" className='botao1' value="salvar" onClick={this.mod}/>
+      </Modal>
+      
+
 
       <Modal
       isOpen={this.state.modalIsOpen}
@@ -134,18 +230,25 @@
       style={customStyles}
       contentLabel="Example Modal"
       >
-       <input type="text" placeholder ="digite o titulo" value={this.state.Titulo} onChange={this.mudarTitulo}/><br/>
+
+      <input type="text" className="titulo" placeholder ="digite o titulo" value={this.state.Titulo} onChange={this.mudarTitulo}/><br/>
       <h ref={subtitle => this.subtitle = subtitle}></h><br/>
 
 
 
+      
+      
+
+
+      
       <DatePicker
       dateFormat="YYYY/MM/DD"
       onChange={this.onChange}
       value={this.state.CalendarioI}
       />
 
-      <TimePicker start="08:00" end="21:00" value={this.state.Hora} onChange={this.mudarHora} step={30} format={24}/>
+      <TimePicker style={estilos} start="00:00" end="23:30" value={this.state.Hora} onChange={this.mudarHora} step={30} format={24}/>
+     
 
       <DatePicker
       selected={this.state.startDate}
@@ -157,7 +260,12 @@
       dateFormat="LT"
       timeCaption="Time"
       />
-       <TimePicker start="08:00" end="21:00" step={30} format={24} value={this.state.Hora2} onChange={this.mudarHora2}/>
+
+
+      
+
+      <TimePicker style={estilos} start="00:00" end="23:30" step={30} format={24} value={this.state.Hora2} onChange={this.mudarHora2}/>
+
 
       <br></br>
       <br></br>
@@ -175,8 +283,8 @@
       defaultView={BigCalendar.Views.WEEK}
       scrollToTime={new Date(1970, 1, 1, 6)}
       defaultDate={new Date()}
-      onSelectEvent={event => alert(event.title)}
-      onSelectSlot={slotInfo => {console.log('fd')}
+      onSelectEvent={event =>this.mudarModal(event)}
+      onSelectSlot={slotInfo => console.log('sfd')
       /*alert(
         `selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
         `\nend: ${slotInfo.end.toLocaleString()}` +
