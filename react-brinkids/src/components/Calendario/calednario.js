02480e7003bf1fe5e455aaef7bfc7bb44d238221
pj300e <38145047+pj300e@users.noreply.github.com>
2018-08-05 16:13:38 -0300

    correção do erro ao editar evento do calendario

--- react-brinkids/src/components/Calendario/calednario.js
+++ react-brinkids/src/components/Calendario/calednario.js
@@ -6,131 +6,133 @@
   */
 
 
-  import React from 'react';
-  import BigCalendar from 'react-big-calendar';
-  import moment from 'moment';
-  import 'moment/locale/pt-br';
-  
-  import events from './events';
-  
-  import DatePicker from 'react-date-picker';
-  import Modal from 'react-modal';
-  import './styles/DatePicker.css';
-  
+import React from 'react';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import 'moment/locale/pt-br';
 
-  import { timeFromInt } from 'time-number';
-  import TimePicker from 'react-bootstrap-time-picker';
-  import '../../assets/style/font-awesome.css';
-  import estilo from './styles/react-big-calendar.css';
+import events from './events';
+
+import DatePicker from 'react-date-picker';
+import Modal from 'react-modal';
+import './styles/DatePicker.css';
 
 
-
-  const estilos = {
-        
-            float: "right",
-    display: "inline-block" ,
-    width: "45% ",
-    height: "45px",
-    padding: "14px 12px",
-    fontSize: "14px",
+import { timeFromInt } from 'time-number';
+import TimePicker from 'react-bootstrap-time-picker';
+import '../../assets/style/font-awesome.css';
+import estilo from './styles/react-big-calendar.css';
 
 
 
+const estilos = {
 
-        
-  };
-  const customStyles = {
-    content : {
-      top                   : '30%',
-      left                  : '50%',
-      right                 : 'auto',
-      bottom                : 'auto',
-      marginRight           : '-50%',
-      transform             : 'translate(-30%, -30%)',
-      overflow              : 'visible',
-      position              : 'absolute',
-
-    }
-  };
-  const iconelixeira = {
-    cursor: "pointer",
+  float: "right",
+  display: "inline-block",
+  width: "45% ",
+  height: "45px",
+  padding: "14px 12px",
+  fontSize: "14px",
 
 
 
-  };
-
-  /*   */
-
-  BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment));
-  var excluirInicial,excluirFinal;
-  
-  class Calendar extends React.Component {
-    constructor(props) {
-      super(props);
-
-      this.state = {
-        showModal: false,
-        modalIsOpen: false,
-        CalendarioF:new Date(),
-        CalendarioI: new Date(),
-        Hora: '0',
-        Hora2:'0',
-        Titulo:'',
-        modalC:false,
 
 
-      };
+};
+const customStyles = {
+  content: {
+    top: '30%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-30%, -30%)',
+    overflow: 'visible',
+    position: 'absolute',
 
-      this.openModal = this.openModal.bind(this);
-      this.afterOpenModal = this.afterOpenModal.bind(this);
-      this.closeModal = this.closeModal.bind(this);
-      this.afterOpenModal = this.afterOpenModal.bind(this);
+  }
+};
+const iconelixeira = {
+  cursor: "pointer",
 
-      this.openModalC = this.openModalC.bind(this);
-      this.afterOpenModalC = this.afterOpenModalC.bind(this);
-      this.closeModalC = this.closeModalC.bind(this);
-      
-      
-      this.mod = this.mod.bind(this);
-      this.mudarTitulo = this.mudarTitulo.bind(this);
-      this.mudarModal = this.mudarModal.bind(this);
 
-      this.ExcluirEvento = this.ExcluirEvento.bind(this);
 
-    }
+};
 
-    componentWillMount() {
-      Modal.setAppElement('body');
-    }
-    
+/*   */
 
-    onChange = CalendarioI => this.setState({ CalendarioI });
-    onChange2 = CalendarioF => this.setState({CalendarioF});
-    
-    mudarHora = Hora => this.setState({Hora});
-    mudarHora2 = Hora2 => this.setState({Hora2});
-    
+BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment));
+var excluirInicial, excluirFinal;
 
-    mudarTitulo(event){
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
 
-      this.setState({Titulo: event.target.value});
-    }
+    this.state = {
+      showModal: false,
+      modalIsOpen: false,
+      CalendarioF: new Date(),
+      CalendarioI: new Date(),
+      Hora: '0',
+      Hora2: '0',
+      Titulo: '',
+      modalC: false,
 
-    openModal() {
-      this.setState({modalIsOpen: true});
-    }
 
-    afterOpenModal() {
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+
+    this.openModalC = this.openModalC.bind(this);
+    this.afterOpenModalC = this.afterOpenModalC.bind(this);
+    this.closeModalC = this.closeModalC.bind(this);
+
+
+    this.mod = this.mod.bind(this);
+    this.mod2 = this.mod2.bind(this);
+    this.mudarTitulo = this.mudarTitulo.bind(this);
+    this.mudarModal = this.mudarModal.bind(this);
+
+    this.ExcluirEvento = this.ExcluirEvento.bind(this);
+
+  }
+
+  componentWillMount() {
+    Modal.setAppElement('body');
+  }
+
+
+  onChange = CalendarioI => this.setState({ CalendarioI });
+  onChange2 = CalendarioF => this.setState({ CalendarioF });
+
+  mudarHora = Hora => this.setState({ Hora });
+  mudarHora2 = Hora2 => this.setState({ Hora2 });
+
+
+  mudarTitulo(event) {
+
+    this.setState({ Titulo: event.target.value });
+  }
+
+  openModal() {
+    this.setState({ modalIsOpen: true });
+  }
+
+  afterOpenModal() {
     // references are now sync'd and can be accessed.
     this.subtitle.style.color = '#f00';
   }
 
   closeModal() {
-    this.setState({modalIsOpen: false});
+    this.setState({ modalIsOpen: false });
+    this.setState({ Titulo: '',Hora: '0',Hora2:'0'});
   }
 
   openModalC() {
-    this.setState({modalC: true});
+    this.setState({ modalC: true });
   }
 
   afterOpenModalC() {
@@ -139,47 +141,79 @@
   }
 
   closeModalC() {
-    this.setState({modalC: false});
+    this.setState({ modalC: false });
+    this.setState({ Titulo: '',Hora: '0',Hora2:'0'});
   }
-  mod(){
+  mod() {
     const titulo = this.state.Titulo;
     const Final = this.state.CalendarioF;
     const Inicial = this.state.CalendarioI;
-    const Anoinicial = Inicial.getFullYear(), MesInicial= Inicial.getMonth(), Diainicial = Inicial.getDate();
-    const Anofinal = Final.getFullYear(), Mesfinal= Final.getMonth(), Diaifinal = Final.getDate();
+    const Anoinicial = Inicial.getFullYear(), MesInicial = Inicial.getMonth(), Diainicial = Inicial.getDate();
+    const Anofinal = Final.getFullYear(), Mesfinal = Final.getMonth(), Diaifinal = Final.getDate();
 
-    const HoraI =timeFromInt(this.state.Hora);
-    const HoraF =timeFromInt(this.state.Hora2);
+    const HoraI = timeFromInt(this.state.Hora);
+    const HoraF = timeFromInt(this.state.Hora2);
+    
     const match = HoraI.match(/([\w\*]+)/g);
     const match2 = HoraF.match(/([\w\*]+)/g);
 
-    events.push({title:titulo,start:new Date(Anoinicial,MesInicial, Diainicial, match[0], match[1], 0),end:new Date(Anofinal,Mesfinal, Diaifinal, match2[0], match2[1], 0),desc:'blabla bla'});
-    this.setState({Titulo:''});
+    events.push({ title: titulo, start: new Date(Anoinicial, MesInicial, Diainicial, match[0], match[1], 0), end: new Date(Anofinal, Mesfinal, Diaifinal, match2[0], match2[1], 0), desc: 'blabla bla' });
+    this.setState({ Titulo: '' });
     this.closeModal();
 
   }
+  mod2() {
 
-  mudarModal(event){
+    const titulo = this.state.Titulo;
+    const Final = this.state.CalendarioF;
+    const Inicial = this.state.CalendarioI;
+    const Anoinicial = Inicial.getFullYear(), MesInicial = Inicial.getMonth(), Diainicial = Inicial.getDate();
+    const Anofinal = Final.getFullYear(), Mesfinal = Final.getMonth(), Diaifinal = Final.getDate();
+    for (var data in events) {
+
+      if (events[data].start === excluirInicial && events[data].end === excluirFinal) {
+        events.splice(data, 1);
+        
+        
+      }
+    }
+    const HoraI = this.state.Hora;
+    const HoraF = this.state.Hora2;
+    
+    const match = HoraI.match(/([\w\*]+)/g);
+    const match2 = HoraF.match(/([\w\*]+)/g);
+
+    events.push({ title: titulo, start: new Date(Anoinicial, MesInicial, Diainicial, match[0], match[1], 0), end: new Date(Anofinal, Mesfinal, Diaifinal, match2[0], match2[1], 0), desc: 'blabla bla' });
+    
+    
+    this.closeModalC();
+
+
+
+  }
+
+  mudarModal(event) {
     excluirInicial = event.start;
     excluirFinal = event.end;
-    const inicial = event.start.getHours() +':'+event.start.getMinutes();
-    const final =event.end.getHours() +':'+event.end.getMinutes();
+    
+    const inicial = event.start.getHours() + ':' + event.start.getMinutes();
+    const final = event.end.getHours() + ':' + event.end.getMinutes();
 
-    this.setState({Titulo: event.title});
-    this.setState({Hora2:final});
-    this.setState({Hora:inicial});
+    this.setState({ Titulo: event.title });
+    this.setState({ Hora2: final });
+    this.setState({ Hora: inicial });
     this.openModalC();
 
   }
 
 
-  ExcluirEvento(event){
+  ExcluirEvento(event) {
 
-    for(var data in events ){
+    for (var data in events) {
 
-      if(events[data].start === excluirInicial && events[data].end === excluirFinal){
+      if (events[data].start === excluirInicial && events[data].end === excluirFinal) {
         events.splice(data, 1);
-        this.setState({Titulo:''});
+        this.setState({ Titulo: '' });
         this.closeModalC();
       }
     }
@@ -190,7 +224,7 @@
   render() {
 
 
-    const teste =(slotInfo) =>{
+    const teste = (slotInfo) => {
 
 
     }
@@ -201,105 +235,106 @@
 
       <div>
 
-      <button className="modal1" type="button" onClick={this.openModal}>adicionar evento</button>
-      
-      <Modal
-      isOpen={this.state.modalC}
-      onAfterOpen={this.afterOpenModalC}
-      onRequestClose={this.closeModalC}
-      style={customStyles}
-      contentLabel="Example Modal2"
-      > 
-      <h ref={subtitle => this.subtitle = subtitle}></h>
-      <div className="glyphicon" style={iconelixeira} onClick={this.ExcluirEvento}>&#xe020;</div>
-      <div className="fa"  style={{cursor: "pointer", float:"right"}} onClick={this.closeModalC}>&#xf00d;</div><br/><br/>
+        <button className="modal1" type="button" onClick={this.openModal}>adicionar evento</button>
+
+        <Modal
+        
+          isOpen={this.state.modalC}
+          onAfterOpen={this.afterOpenModalC}
+          onRequestClose={this.closeModalC}
+          style={customStyles}
+          contentLabel="Example Modal2"
+        >
+          <h ref={subtitle => this.subtitle = subtitle}></h>
+          <div className="glyphicon" style={iconelixeira} onClick={this.ExcluirEvento}>&#xe020;</div>
+          <div className="fa" style={{ cursor: "pointer", float: "right" }} onClick={this.closeModalC}>&#xf00d;</div><br /><br />
 
 
-      <div>Titulo: <input type="text" className="titulo2" placeholder ="digite o titulo" value={this.state.Titulo} onChange={this.mudarTitulo}/><br/>
-      </div>  <br/> 
-      <div> De: <span>{this.state.Hora} ate {this.state.Hora2}</span></div><br/>
-      <input type="button" className='botao1' value="salvar" onClick={this.mod}/>
-      </Modal>
-      
-
-
-      <Modal
-      isOpen={this.state.modalIsOpen}
-      onAfterOpen={this.afterOpenModal}
-      onRequestClose={this.closeModal}
-      style={customStyles}
-      contentLabel="Example Modal"
-      >
-
-      <input type="text" className="titulo" placeholder ="digite o titulo" value={this.state.Titulo} onChange={this.mudarTitulo}/><br/>
-      <h ref={subtitle => this.subtitle = subtitle}></h><br/>
+          <div>Titulo: <input type="text" className="titulo2" placeholder="digite o titulo" value={this.state.Titulo} onChange={this.mudarTitulo} /><br />
+          </div>  <br />
+          <div> De: <span>{this.state.Hora} ate {this.state.Hora2}</span></div><br />
+          <input type="button" className='botao1' value="salvar" onClick={this.mod2} />
+        </Modal>
 
 
 
-      
-      
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
 
-
-      
-      <DatePicker
-      dateFormat="YYYY/MM/DD"
-      onChange={this.onChange}
-      value={this.state.CalendarioI}
-      />
-
-      <TimePicker style={estilos} start="00:00" end="23:30" value={this.state.Hora} onChange={this.mudarHora} step={30} format={24}/>
-     
-
-      <DatePicker
-      selected={this.state.startDate}
-      onChange={this.onChange2}
-      value={this.state.CalendarioF}
-      showTimeSelect
-      showTimeSelectOnly
-      timeIntervals={15}
-      dateFormat="LT"
-      timeCaption="Time"
-      />
-
-
-      
-
-      <TimePicker style={estilos} start="00:00" end="23:30" step={30} format={24} value={this.state.Hora2} onChange={this.mudarHora2}/>
-
-
-      <br></br>
-      <br></br>
-
-      <input type="button" className='botao1' value="salvar" onClick={this.mod}/>
-
-      </Modal>
+          <input type="text" className="titulo" placeholder="digite o titulo" value={this.state.Titulo} onChange={this.mudarTitulo} /><br />
+          <h ref={subtitle => this.subtitle = subtitle}></h><br />
 
 
 
 
-      <BigCalendar
-      selectable
-      events={events}
-      defaultView={BigCalendar.Views.WEEK}
-      scrollToTime={new Date(1970, 1, 1, 6)}
-      defaultDate={new Date()}
-      onSelectEvent={event =>this.mudarModal(event)}
-      onSelectSlot={slotInfo => console.log('sfd')
-      /*alert(
-        `selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
-        `\nend: ${slotInfo.end.toLocaleString()}` +
-        `\naction: ${slotInfo.action}`
-        )*/
-    }
-    eventPropGetter={event => ({
-        style: {
-            backgroundColor: event.start.getDay() < 5
+
+
+
+
+          <DatePicker
+            dateFormat="YYYY/MM/DD"
+            onChange={this.onChange}
+            value={this.state.CalendarioI}
+          />
+
+          <TimePicker style={estilos} start="00:00" end="23:30" value={this.state.Hora} onChange={this.mudarHora} step={30} format={24} />
+
+
+          <DatePicker
+            selected={this.state.startDate}
+            onChange={this.onChange2}
+            value={this.state.CalendarioF}
+            showTimeSelect
+            showTimeSelectOnly
+            timeIntervals={15}
+            dateFormat="LT"
+            timeCaption="Time"
+          />
+
+
+
+
+          <TimePicker style={estilos} start="00:00" end="23:30" step={30} format={24} value={this.state.Hora2} onChange={this.mudarHora2} />
+
+
+          <br></br>
+          <br></br>
+
+          <input type="button" className='botao1' value="salvar" onClick={this.mod} />
+
+        </Modal>
+
+
+
+
+        <BigCalendar
+          selectable
+          events={events}
+          defaultView={BigCalendar.Views.WEEK}
+          scrollToTime={new Date(1970, 1, 1, 6)}
+          defaultDate={new Date()}
+          onSelectEvent={event => this.mudarModal(event)}
+          onSelectSlot={slotInfo => console.log('sfd')
+            /*alert(
+              `selected slot: \n\nstart ${slotInfo.start.toLocaleString()} ` +
+              `\nend: ${slotInfo.end.toLocaleString()}` +
+              `\naction: ${slotInfo.action}`
+              )*/
+          }
+          eventPropGetter={event => ({
+            style: {
+              backgroundColor: event.start.getDay() < 5
                 ? "#ad4ca4"
                 : "#3174ad",
-        }
-    })}
-    />
-    </div>
+            }
+          })}
+        />
+      </div>
 
 
     );
--- react-brinkids/src/components/Dashboard/Dashboard.js
+++ react-brinkids/src/components/Dashboard/Dashboard.js
@@ -17,69 +17,52 @@ class DashBoard extends React.Component {
 		super(props);
 
 		this.state = {
-			
+
 			colaps: true,
-			teste:true,
-			
+			teste: true,
+
 			popup: true,
-			
-			adultospopup:false,
-			
-			Eventos:true,
-			Eventospopup:false,
-			
-			Noticias:true,
-			noticiaspopup:false
-			
+
+			adultospopup: false,
+
+			Eventos: true,
+			Eventospopup: false,
+
+			Noticias: true,
+			noticiaspopup: false
+
 		};
 		this.mudar = this.mudar.bind(this);
 		this.mudar2 = this.mudar2.bind(this);
 		this.mudar3 = this.mudar3.bind(this);
 		this.mudar4 = this.mudar4.bind(this);
 	}
-	mudar(){
-		if(!this.state.colaps){
-			this.setState({colaps:true});
+	mudar() {
+		!this.state.colaps ? this.setState({ colaps: true }) : this.setState({ colaps: false });
+		
+	}
+	mudar2() {
+		!this.state.adultospopup ? this.setState({ adultospopup: true }) : this.setState({ adultospopup: false });
+		
 
-		}else{
-			this.setState({colaps:false});
-		}}
-		mudar2(){
-			if(!this.state.adultospopup){
-				this.setState({adultospopup:true});
-
-			}else{
-				this.setState({adultospopup:false});
-			}
-
-		}
-		mudar3(){
-			if(!this.state.Eventospopup){
-				this.setState({Eventospopup:true});
-
-			}else{
-				this.setState({Eventospopup:false});
-			}            
-
-		}
-		mudar4(){
-
-			if(!this.state.noticiaspopup){
-				this.setState({noticiaspopup:true});
-
-			}else{
-				this.setState({noticiaspopup:false});
-			}
-
-		}
-
-		render() {
+	}
+	mudar3() {
+		!this.state.Eventospopup ? this.setState({ Eventospopup: true }) : this.setState({ Eventospopup: false });
 
 
-			
-			var byCrianca = function(events) {
-				return(
-					<tr key={events.toString()}>
+	}
+	mudar4() {
+
+		!this.state.noticiaspopup ? this.setState({ noticiaspopup: true }) : this.setState({ noticiaspopup: false });
+		
+	}
+	render() {
+
+
+
+		const byCrianca = function (events) {
+			return (
+				<tr key={events.toString()}>
 					<td>{events.id}</td>
 					<td>{events.Nome}</td>
 					<td>{events.Idade}</td>
@@ -87,197 +70,197 @@ class DashBoard extends React.Component {
 					<td>{events.Responsavel}</td>
 					<td>{events.Telefone}</td>
 					<td>{events.Servico}</td>
-					</tr>
-					)
-			}
-			var byAdulto = function(events) {
-				return(
-					<tr key={events.toString()}>
+				</tr>
+			)
+		}
+		const byAdulto = function (events) {
+			return (
+				<tr key={events.toString()}>
 					<td>{events.id}</td>
 					<td>{events.Nome}</td>
 					<td>{events.Aniversariante}</td>
-					</tr>
-					)
-			}
-			var byEvento = function(events) {
-				return(
-					<tr key={events.toString()}>
+				</tr>
+			)
+		}
+		const byEvento = function (events) {
+			return (
+				<tr key={events.toString()}>
 					<td>{events.Titulo}</td>
-					<td>{(events.Start.getHours()+':'+events.Start.getMinutes())}</td>
-					<td>{((events.End.getHours()+2)+':'+(events.End.getMinutes()+11))}</td>
-					</tr>
-					)
-			}
-			var byNoticias = function(events) {
-				return(
-					<tr key={events.toString()}>
+					<td>{(events.Start.getHours() + ':' + events.Start.getMinutes())}</td>
+					<td>{((events.End.getHours() + 2) + ':' + (events.End.getMinutes() + 11))}</td>
+				</tr>
+			)
+		}
+		const byNoticias = function (events) {
+			return (
+				<tr key={events.toString()}>
 					<td>{events.Titulo}</td>
 					<td>{events.Descricao}</td>
-					</tr>
-					)
-				}
-				return (
-
-				<div className = "container-fluid" >
-				
-				{this.state.popup && 
-					
-					<div className="alert alert-success" role="alert">
-					<strong>Tudo ok!</strong> O Limite De Crianças Não foi Atingido.
-					</div>
-				}
-
-
-				{!this.state.popup && 
-					
-					<div className="alert alert-danger" role="alert">
-					<strong>Aviso!</strong> O Limite De Crinaças Foi Atingido.
-					</div>
-
-				}
-				
-				<div className = "sub-heard-part" >
-				<ol className = "breadcrumb m-b-0" >
-				<li > < a href = "/" > Home </a></li >
-				<li > DashBoard </li >
-				</ol >
-				</div>                 
-				<div className="graph"> 
-				<button  className = "sidebar-icon styleButaoMenu" style={{float: 'none'}} onClick={this.mudar}>
-				< span className = "fa fa-bars" > </span>
-				</button>       Crianças 
-				{this.state.colaps && 
-
-					<div className = "graph">
-					<div className ="tables table-responsive">
-					<table className ="table table-hover"> 
-					<thead className = "text-center"> 
-					<tr > 
-					<th>#</th> 
-					<th>Nome</th> 
-					<th>Idade</th> 
-					<th>Tempo</th>
-					<th>Responsavel</th>
-					<th>Telefone</th>
-					<th>Serviço</th>
-					<th></th>
-					</tr>
-					</thead> 
-					<tbody id="CriaTabela">
-					{crianca.map(byCrianca)}
-
-					</tbody>
-					</table>
-					</div>
-					</div>
-					
-					
-				}
-				</div>
-				
-				
-				{this.state.teste && 
-
-					<div className="graph"> 
-					<button  className = "sidebar-icon styleButaoMenu" style={{float: 'none'}} onClick={this.mudar2}>
-					< span className = "fa fa-bars" > </span>
-					</button>       Adultos 
-					{this.state.adultospopup && 
-
-						<div className = "graph">
-						<div className ="tables table-responsive">
-						<table className ="table table-hover"> 
-						<thead className = "text-center"> 
-						<tr > 
-						<th>#</th> 
-						<th>Nome</th> 
-						<th>Aniversariante</th>
-						</tr>
-						</thead> 
-						<tbody id="CriaTabela">
-						{adultos.map(byAdulto)}
-
-						</tbody>
-						</table>
-						</div>
-						</div>
-					}
-
-
-					</div>
-					
-				}
-
-				{this.state.Eventos && 
-
-					<div className="graph"> 
-					<button  className = "sidebar-icon styleButaoMenu" style={{float: 'none'}} onClick={this.mudar3}>
-					< span className = "fa fa-bars" > </span>
-					</button>       Eventos 
-					{this.state.Eventospopup && 
-
-						<div className = "graph">
-						<div className ="tables table-responsive">
-						<table className ="table table-hover"> 
-						<thead className = "text-center"> 
-						<tr> 
-						
-						<th>Titulo</th> 
-						<th>Inicio</th>
-						<th>Fim</th>  
-						</tr>
-						</thead> 
-						<tbody id="CriaTabela">
-						{eventos.map(byEvento)}
-
-						</tbody>
-						</table>
-						</div>
-						</div>
-					}
-
-
-					</div>
-					
-				}
-				{this.state.Noticias && 
-
-					<div className="graph"> 
-					<button  className = "sidebar-icon styleButaoMenu" style={{float: 'none'}} onClick={this.mudar4}>
-					< span className = "fa fa-bars" > </span>
-					</button>     Noticías 
-					{this.state.noticiaspopup && 
-
-						<div className = "graph">
-						<div className ="tables table-responsive">
-						<table className ="table table-hover"> 
-						<thead className = "text-center"> 
-						<tr> 
-						
-						<th>Titulo</th> 
-						<th>Descrição</th> 
-						</tr>
-						</thead> 
-						<tbody id="CriaTabela">
-						{noticias.map(byNoticias)}
-
-						</tbody>
-						</table>
-						</div>
-						</div>
-					}
-
-
-					</div>
-					
-				}
-
-
-				</div>          
-				
-				
-				);
-				
-			}
+				</tr>
+			)
 		}
-		export default DashBoard;
+		return (
+
+			<div className="container-fluid" >
+
+				{this.state.popup &&
+
+					(<div className="alert alert-success" role="alert">
+						<strong>Tudo ok!</strong> O Limite De Crianças Não foi Atingido.
+					</div>)
+				}
+
+
+				{!this.state.popup &&
+
+					(<div className="alert alert-danger" role="alert">
+						<strong>Aviso!</strong> O Limite De Crinaças Foi Atingido.
+					</div>)
+
+				}
+
+				<div className="sub-heard-part" >
+					<ol className="breadcrumb m-b-0" >
+						<li > < a href="/" > Home </a></li >
+						<li > DashBoard </li >
+					</ol >
+				</div>
+				<div className="graph">
+					<button className="sidebar-icon styleButaoMenu" style={{ float: 'none' }} onClick={this.mudar}>
+						< span className="fa fa-bars" > </span>
+					</button>       Crianças
+				{this.state.colaps &&
+
+						(<div className="graph">
+							<div className="tables table-responsive">
+								<table className="table table-hover">
+									<thead className="text-center">
+										<tr >
+											<th>#</th>
+											<th>Nome</th>
+											<th>Idade</th>
+											<th>Tempo</th>
+											<th>Responsavel</th>
+											<th>Telefone</th>
+											<th>Serviço</th>
+											<th></th>
+										</tr>
+									</thead>
+									<tbody id="CriaTabela">
+										{crianca.map(byCrianca)}
+
+									</tbody>
+								</table>
+							</div>
+						</div>)
+
+
+					}
+				</div>
+
+
+				{this.state.teste &&
+
+					(<div className="graph">
+						<button className="sidebar-icon styleButaoMenu" style={{ float: 'none' }} onClick={this.mudar2}>
+							< span className="fa fa-bars" > </span>
+						</button>       Adultos
+					{this.state.adultospopup &&
+
+							(<div className="graph">
+								<div className="tables table-responsive">
+									<table className="table table-hover">
+										<thead className="text-center">
+											<tr >
+												<th>#</th>
+												<th>Nome</th>
+												<th>Aniversariante</th>
+											</tr>
+										</thead>
+										<tbody id="CriaTabela">
+											{adultos.map(byAdulto)}
+
+										</tbody>
+									</table>
+								</div>
+							</div>)
+						}
+
+
+					</div>)
+
+				}
+
+				{this.state.Eventos &&
+
+					<div className="graph">
+						<button className="sidebar-icon styleButaoMenu" style={{ float: 'none' }} onClick={this.mudar3}>
+							< span className="fa fa-bars" > </span>
+						</button>       Eventos
+					{this.state.Eventospopup &&
+
+							(<div className="graph">
+								<div className="tables table-responsive">
+									<table className="table table-hover">
+										<thead className="text-center">
+											<tr>
+
+												<th>Titulo</th>
+												<th>Inicio</th>
+												<th>Fim</th>
+											</tr>
+										</thead>
+										<tbody id="CriaTabela">
+											{eventos.map(byEvento)}
+
+										</tbody>
+									</table>
+								</div>
+							</div>)
+						}
+
+
+					</div>
+
+				}
+				{this.state.Noticias &&
+
+					<div className="graph">
+						<button className="sidebar-icon styleButaoMenu" style={{ float: 'none' }} onClick={this.mudar4}>
+							< span className="fa fa-bars" > </span>
+						</button>     Noticías
+					{this.state.noticiaspopup &&
+
+							(<div className="graph">
+								<div className="tables table-responsive">
+									<table className="table table-hover">
+										<thead className="text-center">
+											<tr>
+
+												<th>Titulo</th>
+												<th>Descrição</th>
+											</tr>
+										</thead>
+										<tbody id="CriaTabela">
+											{noticias.map(byNoticias)}
+
+										</tbody>
+									</table>
+								</div>
+							</div>)
+						}
+
+
+					</div>
+
+				}
+
+
+			</div>
+
+
+		);
+
+	}
+}
+export default DashBoard;
