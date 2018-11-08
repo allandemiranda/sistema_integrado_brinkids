60ec28b4c73719ac9dbcbeb5dac9ec1f0b210406
Marcos Paulo <paulo.marcos0108@live.com>
2018-11-08 17:45:24 -0300

    Adicionando professionalPosition-(models/router)...

---
+++ express-brinkids/models/professional-position-models.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const config = require('../config');
+
+const professionalPositionSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  classes: {
+    type: String,
+    required: true,
+  },
+  functions: {
+    type: String,
+    required: true,
+  },
+});
+
+mongoose.connect(`mongodb://localhost/${config.database}`);
+const professionalPosition = mongoose.model('ProfessionalPosition', professionalPositionSchema);
+
+module.exports = professionalPosition;
