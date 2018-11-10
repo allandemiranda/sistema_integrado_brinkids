36fe0a2e32cd7fa5cac6173d31022e9dcc2e75d7
Gabriel <gabaofogo@hotmail.com>
2018-11-10 13:30:33 -0300

    Adicionadas novas rotas das gavetas

--- express-brinkids/routes/belongings-router.js
+++ express-brinkids/routes/belongings-router.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const belongings = require('../models/belongings-models');
+const Belongings = require('../models/belongings-models');
 
 const router = express.Router();
 
 
 router.get('/', async (req, res) => {
 	try{
-		const belongingsJson = await belongings.find({});
+    const belongingsJson = await Belongings.find({});
+
 		return res.status(200).json(belongingsJson);
 	} catch (err) {
     console.log(err);
@@ -14,24 +15,36 @@ router.get('/', async (req, res) => {
 	}
 });
 
-router.put('/:identifier', async (req, res) => {
+router.put('/', async (req, res) => {
   try {
-    const belongingsModified = await belongings.findByIdAndUpdate(req.params.identifier, {
-    	$set: {
-    		number: req.body.number,
-    	},
-    });
+    const belongings = await Belongings.find({});
 
-    if (!belongingsModified) {
-    	return res.sendStatus(404);
+    belongings[0].number += 1;
+
+    belongings.save();
+
+    return res.sendStatus(204);
+	} catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
+	}
+});
+
+router.delete('/', async (req, res) => {
+  try {
+    const belongings = await Belongings.find({});
+
+    // Impede que exista um número negativo de gavetas quando
+    // não tenha nenhuma gaveta usada
+    if (belongings[0].number === 0) {
+      return res.sendStatus(400);
     }
 
-	} catch (err) {
-    	console.log(err);
-    	return res.sendStatus(500);
-	}
-
-	return res.sendStatus(400);
+    return res.sendStatus(204);
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(500);
+  }
 });
 
 module.exports = router;
