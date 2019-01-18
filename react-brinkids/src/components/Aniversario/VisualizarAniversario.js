5a315ed8517351ae0c35dec89378a77e2d6d7bf1
joao paulo <soarespaulojoao@hotmail.com>
2019-01-18 07:35:19 -0300

    vizualizar aniversarios

--- react-brinkids/src/components/Aniversario/VisualizarAniversario.js
+++ react-brinkids/src/components/Aniversario/VisualizarAniversario.js
@@ -17,78 +17,60 @@ class VisualizarAniversario extends React.Component {
         super(props);
         this.state = {
             list: [],
+            Page:"Lista",
         }
 
 
 
     }
     render() {
-        return (
-            <div className="container-fluid" >
-                <div className="sub-heard-part" >
-                    <ol className="breadcrumb m-b-0" >
-                        <li > < a href="/" > Home </a></li >
-                        <li >Visualizar Aniversarios </li>
-                    </ol >
-                </div>
-                <div className="graph-visual" >
-                    <h3 className="inner-tittle" >Aniversarios </h3>
-                    <div className="graph" >
-                        <table className="table">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Titulo</th>
-                                    <th >Aniversariante</th>
-                                    <th >Responsavel</th>
-                                    <th> Data </th>
-                                    <th></th>
-                                </tr>
-                            </thead>
+        if (this.state.Page === "Lista") {
+            return (
+                <div className="container-fluid" >
+                    <div className="sub-heard-part" >
+                        <ol className="breadcrumb m-b-0" >
+                            <li > < a href="/" > Home </a></li >
+                            <li >Visualizar Aniversarios </li>
+                        </ol >
+                    </div>
+                    <div className="graph-visual">
+                        <h3 className="inner-tittle">Aniversarios </h3>
+                        <div className="graph" >
+                            <table className="table">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Titulo</th>
+                                        <th >Aniversariante</th>
+                                        <th >Responsavel</th>
+                                        <th> Data </th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
 
-                            <tbody>
-                                {this.state.list.map((findAdult, indice) => {
+                                <tbody>
+                                    {this.state.list.map((findAdult, indice) => {
 
 
-                                    return (
-                                        <tr key={indice + 1}>
-                                            <th scope="row">{indice + 1}</th>
-                                            <td > {findAdult.name.firstName + ' ' + findAdult.name.surName} </td>
-                                            <td > {findAdult.cpf} </td>
-                                            <td className="text-center">
-                                                <div className="form-group">
+                                        return (
+                                            <tr key={indice + 1}>
+                                                <th scope="row">{indice + 1}</th>
+                                                <td > {findAdult.title} </td>
+                                                <td > {findAdult.birthdayPerson.name} </td>
+                                                <td > {findAdult.cpf} </td>
+                                                <td > {findAdult.start} </td>
 
-                                                    <select className="form-control" style={{ height: 47 + 'px' }} id="exampleFormControlSelect2" onChange={this.changueselect}>
+                                            </tr>
+                                        );
+                                    })}
+                                </tbody>
+                            </table>
 
-                                                        {this.state.listadecargos.map((cargos, indice1) => {
-                                                            if (findAdult.identifierEmployee.employeeData.officialPosition === cargos._id) {
-                                                                console.log(cargos.name)
-                                                                console.log(findAdult.identifierEmployee.employeeData.officialPosition, "----", cargos._id)
-                                                                return (
-
-                                                                    <option key={indice1 + 1} selected value={[indice, indice1]}  >{cargos.name}</option>
-                                                                );
-                                                            } else {
-                                                                return (
-                                                                    <option key={indice1 + 1} value={[indice, indice1]}>{cargos.name}</option>
-
-                                                                );
-                                                            }
-
-                                                        })}
-                                                    </select>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </table>
-                        
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
     }
 }
 export default VisualizarAniversario; 
